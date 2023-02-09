3d3a075c7e40a9f6174e83a189c8a4801fe463ef
Prajwol Rai <raiprajwol123@gmail.com>
2023-02-09 22:42:56 +0545

    portfolio

---
+++ JS/assignements.js
@@ -0,0 +1,33 @@
+let country = "Nepal";
+let continent = "Asia";
+// let population = 30;
+// console.log(country);
+// console.log(continent);
+// console.log(population);
+// /*
+// let isIsland = false;
+// let language;
+// console.log(typeof isIsland);
+// console.log(typeof population);
+// console.log(typeof country);
+// console.log(typeof language);
+// */
+/*
+language = "Nepali";
+const country = "Nepal";
+const continent = "Asian";
+const isIsland = false;
+isIsland = true;*/
+
+let population
+console.log(population / 2);
+population++;
+console.log(population);
+console.log(population>6);
+console.log(population<33);
+const description=
+country +" is in "+continent+", and its "+population+" million people speak "+language;
+console.log(description);
+
+
+ 
---
+++ week 9/js/week9.js
@@ -0,0 +1,25 @@
+/*console.log("supp world");
+var a=6;
+var b=6;
+var sum=a+b;
+const PI=3.14;
+sum=a+b+PI;
+console.log(sum);
+alert(sum);*/
+
+/*function printTemp(){
+    var celsius = 100;
+    var temp=(celsius*9/5)+32;
+    console.log(temp);
+    return temp;
+}
+printTemp()*/ 
+/*document.getElementById("paragraph").innerText("Supp world");*/
+
+/*var username= window.prompt("What is your name : ")
+console.log(username)*/
+
+document.getElementById("myButton").onclick=function(){
+    let username=document.getElementById("myText").value;
+    console.log(username)
+}
